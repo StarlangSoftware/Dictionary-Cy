8a637e4d7b5b1c45cd8e550569ccf835abe68fdd
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-05-22 21:39:19 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 from pathlib import Path
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 from Cython.Build import cythonize
 
 setup(
     ext_modules=cythonize(["Dictionary/*.pyx", "Dictionary/Trie/*.pyx", "Language/*.pyx", "Syllibification/*.pyx"],
                           compiler_directives={'language_level': "3"}),
     name='NlpToolkit-Dictionary-Cy',
-    version='1.0.14',
+    version='1.0.15',
     packages=['Language', 'Dictionary', 'Dictionary.data', 'Dictionary.Trie', 'Syllibification'],
     package_data={'Language': ['*.pxd', '*.pyx', '*.c'],
                   'Dictionary': ['*.pxd', '*.pyx', '*.c', '*.py'],
